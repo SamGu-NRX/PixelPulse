867b64aac54674c21130358e19208d28d1cdeeae
Cooperzilla <cooperye09@gmail.com>
2024-04-30 20:45:43 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeeeeeeeee

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs'
 
 const app = express()
 
+var __dirname = fs.__dirname
+
 app.get('/', function (req, res) {
-    res.sendFile(path.sendFile('./index.html', root));
+    res.sendFile(path.join(__dirname, './index.html').toString());
 });
 
 app.listen(9000);
