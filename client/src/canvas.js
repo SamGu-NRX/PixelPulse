481fee0d394e7bbb442570f0bec139c455852496
SamGu-NRX <sgu07966@gmail.com>
2024-05-02 12:42:18 -0500

    Fix bugs

--- client/src/canvas.js
+++ client/src/canvas.js
@@ -1,21 +1,21 @@
-import React, { useRef, useEffect } from 'react';
-import io from 'socket.io-client';
+// import React, { useRef, useEffect } from 'react';
+// import io from 'socket.io-client';
 
-// const socket = io('http://localhost:3000'); // Connect to server
+// // const socket = io('http://localhost:3000'); // Connect to server
 
-function Canvas() {
-    const canvasRef = useRef(null);
+// function Canvas() {
+//     const canvasRef = useRef(null);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+//     useEffect(() => {
+//         const canvas = canvasRef.current;
+//         const context = canvas.getContext('2d');
 
-        // Basic drawing on the canvas here
-        context.fillStyle = 'red';
-        context.fillRect(10, 10, 50, 50);
-    }, []);
+//         // Basic drawing on the canvas here
+//         context.fillStyle = 'red';
+//         context.fillRect(10, 10, 50, 50);
+//     }, []);
 
-    return <canvas ref={canvasRef} width={600} height={400} />;
-}
+//     return <canvas ref={canvasRef} width={600} height={400} />;
+// }
 
-export default Canvas;
+// export default Canvas;
